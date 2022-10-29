4913cba5133d1290e880d62ae0a88c3089c65d2c
Nataliya127 <sayp35@ya.ru>
2022-10-29 16:39:30 +0300

    Initial commit

--- Program.cs
+++ Program.cs
@@ -1,45 +1,5 @@
-﻿/* ЗАДАЧА:
-Написать программу, которая из имеющегося массива строк формирует массив из строк, 
-длина которых либо меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, 
-либо задать на старте выполнения алгоритма.
-При решение не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
-*/
-
-
-System.Console.Write("Введите количество элементов ");
-int size = System.Convert.ToInt32(System.Console.ReadLine());
-
-string[] arrayStrings = new string[size];
-for (int i = 0; i < size; i++)
-{
-    System.Console.WriteLine($"Введите {i+1}-й элемент: ");
-    string element = System.Convert.ToString(System.Console.ReadLine());
-    arrayStrings[i] = element;
-}
-
-string[] arrayElements = new string[size];
-int len = 3;
-int pos = 0;
-
-for (int e = 0; e < size; e++)
-{
-    if (arrayStrings[e].Length <= len)
-    {
-        arrayElements[pos] = arrayStrings[e];
-        pos++;
-    }
-}
-
-System.Console.WriteLine();
-PrintArray(arrayElements);
-
-
-void PrintArray(string[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        System.Console.Write(array[i] + " ");
-    }
- 
-    System.Console.WriteLine();
-}
+﻿// ЗАДАЧА:
+//Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+//длина которых либо меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, 
+//либо задать на старте выполнения алгоритма.
+//При решение не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
